ad55ab11ba459715bc2669034964ee5c1a96d61f
Ace <huyhoang.sdv@gmail.com>
2024-03-22 15:46:50 +0700

    Them trang gioi thieu

---
+++ introduce.php
@@ -0,0 +1,30 @@
+<?php 
+    include './layouts/header.php'; 
+    include './inc/search.php';
+?>
+<h3 class="text-center mt-3 text-uppercase" style="font-weight: bold;">GIỚI THIỆU</h3>
+<!-- Product Section Begin -->
+<section class="product spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12">
+                <p style="font-size: 2rem;line-height:1.5;text-align:justify;">
+                    <b>Ogani</b> là một trong những website chuyên cung cấp các loại rau, củ, quả sạch cho các hộ gia đình. Sử dụng các công nghệ tiên tiến
+                    để trồng trọt, canh tác cho nên các loại rau, củ, quả được có chất lượng rất tốt và an toàn với người tiêu dùng
+                </p>
+                <p style="font-size: 2rem;line-height:1.5;text-align:justify;">
+                    Thông tin liên hệ
+                </p>
+                <p style="font-size: 1.3rem;line-height:1.5;text-align:justify;">
+                    <i class="fa fa-map-marker" aria-hidden="true"></i> Địa chỉ: Hà Nội
+                    <br>
+                    <i class="fa fa-phone" aria-hidden="true"></i> Số điện thoại: +01 2345 6789 
+                    <br>
+                    <i class="fa fa-envelope" aria-hidden="true"></i> Email: ogani@gmail.com
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Product Section End -->
+<?php include './layouts/footer.php'; ?>
